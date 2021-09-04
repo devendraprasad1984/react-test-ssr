a5c1aa23c11ede6f79429aceb455c2a4baa6ee51
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-04 19:56:35 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use('^/$', (req, res, next) => {
         }
         return res.send(data.replace(
             '<div id="root"</div>',
-            `<div id="root">${ReactDOMServer.renderToString(<App/>)}</div>`)
+            `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`)
         )
     })
 })
