c0b2ca504883401093426a30a7eb321382ab3c18
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-04 19:58:31 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     const [counter, setCounter] = useState(0)
     return (
         <div className="App">
-            <h1>Basic Counter App...</h1>
+            <h1>Basic Counter App.., fine</h1>
             <h2>{counter}</h2>
             <button onClick={() => setCounter(counter + 1)}>+</button>
             <button onClick={() => setCounter(counter - 1)}>-</button>
