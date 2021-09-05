ac4d2dbcb963bc9fd7f38d29396910f01ec96013
Devendra Prasad <devendraprasad1984@gmail.com>
2021-09-05 14:30:25 +0530

    updates

--- ssr-webpack-firebase/server.js
+++ ssr-webpack-firebase/server.js
@@ -1,16 +1,17 @@
 import functions from 'firebase-functions'
 import React from "react";
 import {renderToString} from 'react-dom/server'
-
+import fs from 'fs'
 import App from './src/app'
 import express from 'express'
 
-
+const index=fs.readFileSync(__dirname+'index.html','utf8')
 const app = express()
 app.get('**', (req, res) => {
     const html = renderToString(<App/>)
+    const finalHtml=html.replace('___ROOT___',`${html}`)
     res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
-    res.send(html)
+    res.send(finalHtml)
 })
 
 export let ssrapp = functions.https.onRequest(app)
